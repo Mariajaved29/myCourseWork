c2d498735179930740c8de82a454e2332e989a72
Maria Javed <mariajaved722@gmail.com>
2021-02-27 16:41:17 +0500

    Second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function sum()
+ {
+    num1 = +prompt("Enter any number");
+    num2 = +prompt("Enter any number");
+    sum = num1 + num2;
+    document.write("Sum of " +num1+ " and " +num2+ " is " +sum);
+    return sum;
+ }
+ sum();
